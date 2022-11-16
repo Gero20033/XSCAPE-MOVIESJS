74e63579413066dae0e0f62a002c57d23ab8efd8
Gero20033 <111603858+Gero20033@users.noreply.github.com>
2022-11-16 15:05:48 -0300

    Soluciones
    Intentando solucionar problema con el search result y con las apis de "mejores valoradas" y "peliculas populares". Cuando intento que me tire la informacion y los posters de los dos a la vez no funciona, solo uno a la vez muestra la informacion que pedi.

---
+++ js/lomasvisto.js
@@ -0,0 +1,27 @@
+let categories=document.querySelector(".peliculas23.toprated");
+    categories.style.display="flex";
+    categories.style.flexWrap="wrap";
+
+let info = ''
+
+//let .peliculas23=""
+
+fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=5f0f24101490e6eaa3bbc2c5c64c7369")
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data);
+    for(let i=12; i < data.results.length; i++){
+        info +=`
+        <article>
+        <img src= 'https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}' alt='${data.results[i].original_title}'/>
+        <p> ${data.results[i].original_title}</p>
+        </article>`
+    }
+    categories.innerHTML= info
+})
+.catch(function(error){
+    console.log("Error: " + error);
+})
+
