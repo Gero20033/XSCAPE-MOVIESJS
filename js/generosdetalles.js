c461d5607740fafdfb42d2907d226f029b33d15b
Gero20033 <111603858+Gero20033@users.noreply.github.com>
2022-11-19 22:34:15 -0300

    Ultimo pull del dia
    asi queda al dia todo el progreso

---
+++ js/generosdetalles.js
@@ -0,0 +1,28 @@
+let categoriesPopulares=document.querySelector(".popu")
+    categoriesPopulares.style.display="flex";
+    categoriesPopulares.style.flexWrap="wrap";
+
+let infoPopulares = ''
+
+fetch("https://api.themoviedb.org/3/discover/movie?api_key=5f0f24101490e6eaa3bbc2c5c64c7369")
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data);
+    for(let i=0; i < data.results.length; i++){
+        infoPopulares +=`
+        <a href="movie-detail2.html?id=${data.results[i].id}">
+        <article>
+        <img src= 'https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}' alt='${data.results[i].original_title}'/>
+        <h4> ${data.results[i].original_title}</h4>
+        <h4> ${data.results[i].release_date}</h4>
+        </article>
+        </a>
+        `
+    }
+    categoriesPopulares.innerHTML= infoPopulares
+})
+.catch(function(error){
+    console.log("Error: " + error);
+})
