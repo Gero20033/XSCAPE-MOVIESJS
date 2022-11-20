2c31508b89a77535aaee2d990c5357b831868a41
Gero20033 <111603858+Gero20033@users.noreply.github.com>
2022-11-20 20:30:29 -0300

    Generos terminado
    Al hacer click en generos me muestra las peliculas de ese genero. El css no esta hecho pero por falta de tiempo por el momento queda asi. Ahora a intentar hacer el boton de recomendaciones y todo favoritos

--- js/generosdetalles.js
+++ js/generosdetalles.js
@@ -1,10 +1,11 @@
 let generosDetalles=document.querySelector(".detallegenero")
-    generosDetalles.style.display="flex";
-    generosDetalles.style.flexWrap="wrap";
+let parametros = new URLSearchParams(window.location.search);
+let id = parametros.get("id");
+
 
 let infoDetailgenre = ''
 
-fetch("https://api.themoviedb.org/3/discover/movie?api_key=5f0f24101490e6eaa3bbc2c5c64c7369")
+fetch(`https://api.themoviedb.org/3/discover/movie?api_key=5f0f24101490e6eaa3bbc2c5c64c7369&with_genres=${id}`)
 .then(function(response){
     return response.json()
 })
@@ -12,7 +13,13 @@ fetch("https://api.themoviedb.org/3/discover/movie?api_key=5f0f24101490e6eaa3bbc
     console.log(data);
     for(let i=0; i < data.results.length; i++){
         infoDetailgenre +=`
-        
+        <a href="movie-detail2.html?id=${data.results[i].id}">
+        <article>
+        <img src= 'https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}' alt='${data.results[i].original_title}'/>
+        <h4> ${data.results[i].original_title}</h4>
+        <h4> ${data.results[i].release_date}</h4>
+        </article>
+        </a>
         `
     }
     generosDetalles.innerHTML= infoDetailgenre
@@ -20,3 +27,4 @@ fetch("https://api.themoviedb.org/3/discover/movie?api_key=5f0f24101490e6eaa3bbc
 .catch(function(error){
     console.log("Error: " + error);
 })
+
